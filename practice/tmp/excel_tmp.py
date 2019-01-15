fabc0cb19535bc541423ba3b0f3d13a3836a5fcc
JarvisSu <Jarvis_su@126.com>
2019-01-15 22:34:43 +0800

    绘制图形demo

--- practice/tmp/excel_tmp.py
+++ practice/tmp/excel_tmp.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-xls_file = pd.ExcelFile("C:/Users/SuJie/Desktop/tmp/2018-12-06OCR.xls")
+xls_file = pd.ExcelFile("../3/data/catering_dish_profit.xls")
 print(xls_file.sheet_names)
 
 table1 = xls_file.parse("Sheet1")
-# print(table1)
+print(table1)
